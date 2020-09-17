2b6d4f463799a96151e90bdf753c87a5bff29212
Feldor-Lex <38927234+Feldor-Lex@users.noreply.github.com>
2020-09-18 00:45:58 +1200

    Basic package structure
    Code set up to match the specifications in the brief in terms of package structure, no actual code.

---
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
@@ -0,0 +1,24 @@
+package nz.ac.vuw.ecs.swen225.gp20.application;
+
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+import nz.ac.vuw.ecs.swen225.gp20.persistence.Persistence;
+import nz.ac.vuw.ecs.swen225.gp20.recnplay.RecordAndPlay;
+import nz.ac.vuw.ecs.swen225.gp20.render.Render;
+
+public class Main {
+    private static Maze maze;//These are only static for now, cna change if it proves to be an issue.
+    private static Render render;//That said, these are never going to be deleted and replaced, so static may be better.
+    private static RecordAndPlay recNPlay;
+    private static Persistence persist;
+
+
+    public static void main(String[] args){
+        persist = new Persistence();//Persistance may need to be a parameter for Maze/Record so set it up first! Change if necessary.
+        maze = new Maze();
+        render = new Render();
+        recNPlay = new RecordAndPlay();
+
+    }
+
+
+}
