b734dd753bce7df2156801f230cf117d7de19e8b
Melissa Lok <lokmeli@ecs.vuw.ac.nz>
2020-09-21 17:27:19 +1200

    Implement base skeleton code for replay. Many methods needed to continue.

---
+++ src/nz/ac/vuw/ecs/swen225/gp20/recnplay/Replay.java
@@ -0,0 +1,26 @@
+package nz.ac.vuw.ecs.swen225.gp20.recnplay;
+
+// TODO all imports needed:
+//         1. maze's board and player (???)
+//         2. renderer
+
+import nz.ac.vuw.ecs.swen225.gp20.application.Main;
+
+public class Replay {
+    private Main game;
+
+    public Replay(Main game) {
+        this.game = game;
+    }
+
+    /**
+     * This method handles the actual game replays.
+     */
+    public void replayGame() {
+        // TODO the whole method :(
+    }
+    
+    //todo need to make a method to get moves on the map
+    //     maybe from ticks to moves?
+    
+}
