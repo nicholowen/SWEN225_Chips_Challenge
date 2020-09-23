856d6c571d5d969882ab7f0e23caa6fa4450c3d6
Feldor-Lex <38927234+Feldor-Lex@users.noreply.github.com>
2020-09-24 09:20:49 +1200

    Refactoring Record and play
    Ensuring project compiles now that Record-And-Play has been split into two seperate classes.

--- src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
@@ -3,12 +3,14 @@ package nz.ac.vuw.ecs.swen225.gp20.application;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
 import nz.ac.vuw.ecs.swen225.gp20.persistence.Persistence;
 import nz.ac.vuw.ecs.swen225.gp20.recnplay.*;
+import nz.ac.vuw.ecs.swen225.gp20.recnplay.Record;
 import nz.ac.vuw.ecs.swen225.gp20.render.Render;
 
 public class Main {
     private static Maze maze;//These are only static for now, cna change if it proves to be an issue.
     private static Render render;//That said, these are never going to be deleted and replaced, so static may be better.
-    private static RecordAndPlay recNPlay;
+    private static Record rec;
+    private static Replay repl;
     private static Persistence persist;
 
 
@@ -16,7 +18,8 @@ public class Main {
         persist = new Persistence();//Persistance may need to be a parameter for Maze/Record so set it up first! Change if necessary.
         maze = new Maze(persist);
         render = new Render();
-        recNPlay = new RecordAndPlay();
+        rec = new Record();
+        repl = new Replay();
 
     }
 
