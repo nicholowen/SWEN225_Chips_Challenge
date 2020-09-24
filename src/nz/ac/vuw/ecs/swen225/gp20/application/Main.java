ec8ab392e6b7daf80ed0761c77d60a99305cddc6
Maiza <rehanmain@myvuw.ac.nz>
2020-09-24 16:11:17 +1200

    Added Javadoc and fixed 1 bug

--- src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
@@ -12,17 +12,21 @@ public class Main {
     private static Record rec;
     private static Replay repl;
     private static Persistence persist;
+    private boolean gameEnded;
 
 
-    public static void main(String[] args){
+    public void main(String[] args){
         persist = new Persistence();//Persistance may need to be a parameter for Maze/Record so set it up first! Change if necessary.
         maze = new Maze(persist);
         render = new Render();
-        rec = new Record();
-        repl = new Replay();
+        rec = new Record(this);
+        repl = new Replay(this);
 
     }
 
+    /**
+     * Tick based loop. The main game runs on this loop.
+     */
     public void play() {
     	while(true){
     		if(gameEnded) break;
