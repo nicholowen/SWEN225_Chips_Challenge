13b96db6667228732650b8efef7e6d708733c769
Maiza Rehan <rehanmaiz@ecs.vuw.ac.nz>
2020-09-25 15:32:58 +1200

    Separate class for KeyListeners

---
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/KeyListeners.java
@@ -0,0 +1,67 @@
+package nz.ac.vuw.ecs.swen225.gp20.application;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import nz.ac.vuw.ecs.swen225.gp20.render.Render;
+
+
+public class KeyListeners implements KeyListener{
+
+	private boolean recording = false;
+	private String direction = null;
+	
+	public KeyListeners(Render render) {
+		render.getFrame().addKeyListener(this);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		char key = e.getKeyChar();
+		switch (key) {
+		case 'w':
+			this.direction = "up";
+		case 'a':
+			this.direction = "left";
+		case 's':
+			this.direction = "down";
+		case 'd':
+			this.direction = "right";
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		this.direction = null;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		char key = e.getKeyChar();
+		switch (key) {
+		case 'r':
+			if(recording) recording = true;
+			else recording = false;
+		}
+	}
+	
+    /*===================================================
+    *	Getters and Setters
+    * ===================================================*/
+    public boolean isRecording() {
+		return recording;
+	}
+
+	public void setRecording(boolean recording) {
+		this.recording = recording;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+}
