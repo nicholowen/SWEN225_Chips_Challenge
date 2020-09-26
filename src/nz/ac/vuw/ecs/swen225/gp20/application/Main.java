20fd8e1040f447d95b0132d7b800a11966da73cd
Maiza Rehan <rehanmaiz@ecs.vuw.ac.nz>
2020-09-26 15:37:46 +1200

    Formatting and now updating maze before updating render every tick

--- src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
@@ -6,38 +6,41 @@ import nz.ac.vuw.ecs.swen225.gp20.recnplay.*;
 import nz.ac.vuw.ecs.swen225.gp20.render.Render;
 
 public class Main {
-    private static Maze maze;//These are only static for now, cna change if it proves to be an issue.
-    private static Render render;//That said, these are never going to be deleted and replaced, so static may be better.
-    private static RecordAndPlay rnp;
-    private static Persistence persist;
-    private static KeyListeners keyListeners;
-    private static boolean gameEnded;
+	private static Maze maze;// These are only static for now, cna change if it proves to be an issue.
+	private static Render render;// That said, these are never going to be deleted and replaced, so static may be
+									// better.
+	private static RecordAndPlay rnp;
+	private static Persistence persist;
+	private static KeyListeners keyListeners;
+	private static boolean gameEnded;
 
-
-    public static void main(String[] args){
-        persist = new Persistence();//Persistance may need to be a parameter for Maze/Record so set it up first! Change if necessary.
-        maze = new Maze(persist);
-        render = new Render();
-        rnp = new RecordAndPlay();
-        keyListeners = new KeyListeners(render);
+	public static void main(String[] args) {
+		persist = new Persistence();// Persistance may need to be a parameter for Maze/Record so set it up first!
+									// Change if necessary.
+		maze = new Maze(persist);
+		render = new Render();
+		rnp = new RecordAndPlay();
+		keyListeners = new KeyListeners(render);
 		play();
-    }
+	}
 
-    /**
-     * Tick based loop. The main game runs on this loop.
-     */
-    public static void play() {
-        maze.loadMaze(1);
-        render.init(maze.getBoard());
-    	while(true){
-    		if(gameEnded) break;
-            render.update(maze.getBoard(), maze.getActors());
-            maze.tick(keyListeners.getDirection());
-    		long start = System.currentTimeMillis(); 
-    		while(true) {
-    			int delay = 33;
-    			if(System.currentTimeMillis() >= start + delay) break; // wait 33 milli
-    		}
-    	}
-    }
+	/**
+	 * Tick based loop. The main game runs on this loop.
+	 */
+	public static void play() {
+		maze.loadMaze(1);
+		render.init(maze.getBoard());
+		while (true) {
+			if (gameEnded)
+				break;
+			maze.tick(keyListeners.getDirection());
+			render.update(maze.getBoard(), maze.getActors());
+			long start = System.currentTimeMillis();
+			while (true) {
+				int delay = 33;
+				if (System.currentTimeMillis() >= start + delay)
+					break; // wait 33 milli
+			}
+		}
+	}
 }
