5ff8faec9336751b1bff1be1a91204c467e7df9f
Owen Nicholson <nicholowen@ecs.vuw.ac.nz>
2020-09-27 21:05:04 +1300

    Refactor Render.java to allow Application module to manage the rendering panels

--- src/nz/ac/vuw/ecs/swen225/gp20/render/Render.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/Render.java
@@ -1,42 +1,17 @@
 package nz.ac.vuw.ecs.swen225.gp20.render;
-
-import nz.ac.vuw.ecs.swen225.gp20.maze.Actor;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Cell;
 import nz.ac.vuw.ecs.swen225.gp20.maze.RenderTuple;
 import nz.ac.vuw.ecs.swen225.gp20.render.Renderer.GamePanel;
 import nz.ac.vuw.ecs.swen225.gp20.render.Renderer.ScorePanel;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 public class Render {
 
-  JFrame frame;
-  Canvas canvas;
   GamePanel gp;
   ScorePanel sp;
 
-  public Render(){
-    frame = new JFrame();
-    frame.setResizable(false);
-    frame.setMinimumSize(new Dimension(800, 500));
-    frame.setLayout(new GridBagLayout());
-    gp = new GamePanel();
-    GridBagConstraints gbc = new GridBagConstraints();
-    gbc.gridx = 0;
-    frame.add(gp);
-    frame.add(new ScorePanel());
-    gbc.gridx = 1;
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
-    frame.pack();
-
-
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-//    gp.run();
+  public Render(GamePanel gp, ScorePanel sp){
+    this.gp = gp;
+    this.sp = sp;
   }
 
   public void update(RenderTuple tuple){
@@ -46,11 +21,5 @@ public class Render {
   public void init(Cell[][] cells){
     gp.initAnimationObjects(cells);
   }
-
-  public JPanel getGp(){
-    return gp;
-  }
-
-
 
 }
