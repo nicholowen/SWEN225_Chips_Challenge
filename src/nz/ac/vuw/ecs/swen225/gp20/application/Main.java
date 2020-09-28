fe573ab50f03a7012e596897f7faeef0c5a63e0e
Maiza Rehan <rehanmaiz@ecs.vuw.ac.nz>
2020-09-28 23:30:23 +1300

    added checkUpdates() method to check for user input updates from gui every tick. this method will also handle saving, loading and pausing

--- src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/Main.java
@@ -1,5 +1,7 @@
 package nz.ac.vuw.ecs.swen225.gp20.application;
 
+import java.io.IOException;
+
 import com.google.gson.annotations.JsonAdapter;
 
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
@@ -25,6 +27,7 @@ public class Main {
 
     private boolean gameEnded;
     private int timeRemaining;
+    private String direction = null;
 
     public Main() {
 
@@ -39,7 +42,8 @@ public class Main {
         timeRemaining = maze.loadMaze(1);
         render.init(maze.getBoard());
         while (!gameEnded) {
-            render.update(maze.tick(gui.getDirection()), timeRemaining);
+            checkUpdates();
+            render.update(maze.tick(direction), timeRemaining);
 
             long startTick = System.currentTimeMillis();
             while (true) {
@@ -47,19 +51,61 @@ public class Main {
                 if (System.currentTimeMillis() >= startTick + tickDelay)
                     break; // wait 33 milli
             }
-            while (gui.isPaused()) {
-                boolean paused = gui.isPaused();
-                System.out.println(paused);
-                if (!paused)
-                    break;
-            }
             if (System.currentTimeMillis() >= start + delay) {
                 start = System.currentTimeMillis();
                 timeRemaining--; // timeRemaining goes down every second
             }
         }
     }
-    
+
+    /**
+     * Checks for user input updates from the GUI class. Handles saving, loading,
+     * updating direction and pausing behaviours.
+     */
+    public void checkUpdates() {
+        while (gui.isPaused()) {
+            boolean paused = gui.isPaused();
+            System.out.println(paused);
+            if (!paused)
+                break;
+        }
+        // Update direction
+        direction = gui.getDirection();
+        // Check if user wants to save
+        if (gui.isSaving()) {
+//            try {
+//                persist.saveGameState(this);
+            System.out.println("Saving");
+            gui.saved(true);
+//            } catch (IOException e) {
+//                gui.saved(true);    // true for now (will change later)
+//                System.out.println("Error With Saving State");
+//            }
+        }
+        // Check if user wants to load game, and which kind of state
+        if (gui.getLoadState() != null) {
+            if (gui.getLoadState().equalsIgnoreCase("resume")) {
+                System.out.println("resume");
+                gui.setLoadState(null);
+                // RESUME A GAME
+            } else if (gui.getLoadState().equalsIgnoreCase("unfinished")) {
+                System.out.println("unfinished");
+                // LOAD GAME AT LAST UNFINISHED LEVEL
+                gui.setLoadState(null);
+            } else if (gui.getLoadState().equalsIgnoreCase("lvl 1")) {
+                System.out.println("lvl 1");
+                // LOAD GAME AT LEVEL 1
+                gui.setLoadState(null);
+            }
+        }
+
+    }
+
+    /**
+     * Gets the maze.
+     *
+     * @return the maze
+     */
     public Maze getMaze() {
         return maze;
     }
