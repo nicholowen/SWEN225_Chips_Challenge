827a789fcd5b4c3d3e5cb9eb5e7e6badff0802ad
Feldor-Lex <38927234+Feldor-Lex@users.noreply.github.com>
2020-09-29 16:05:13 +1300

    RenderTuple now carries more information
    RenderTuple carries the player inventory and whether or not the player is standing on an "info" tile. Also carries the info in question.

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/RenderTuple.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/RenderTuple.java
@@ -4,11 +4,17 @@ public class RenderTuple {
 	
 	private Actor[] actorList;
 	private Cell[][] board;
+	private String[] inventory;
+	private boolean playerStandingOnInfo;
+	String info;
 	
 	
-	public RenderTuple(Actor[] aList, Cell[][] b) {
+	public RenderTuple(Actor[] aList, Cell[][] b, String[] inv, boolean playerOnInfo, String info) {
 		actorList=aList;
 		board=b;
+		playerStandingOnInfo=playerOnInfo;
+		this.info=info;
+		inventory=inv;
 	}
 	
 	public Actor[] getActors() {
@@ -17,6 +23,16 @@ public class RenderTuple {
 	
 	public Cell[][] getCells(){
 		return board;
+	}
+	
+	public String[] getInventory() {
+		return inventory;
+	}
+	public boolean isPlayerOnInfo() {
+		return playerStandingOnInfo;
+	}
+	public String getInfo() {
+		return info;
 	}
 	
 }
