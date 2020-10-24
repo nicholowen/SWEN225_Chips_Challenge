c9252c2c26ed32b754ddec55e8e44d2d53c78fce
Maiza <rehanmain@myvuw.ac.nz>
2020-10-24 15:02:09 +1300

    removed system print statements

--- src/nz/ac/vuw/ecs/swen225/gp20/application/MouseManager.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/application/MouseManager.java
@@ -40,7 +40,6 @@ public class MouseManager implements MouseListener {
      */
     @Override
     public void mousePressed(MouseEvent mouseEvent) {
-        System.out.println("pressed");
         String name = mouseEvent.getComponent().getName();
         if (gui.getGameState() == 4) {
             if (name.equals("pause") || name.equals("record")) {
@@ -61,7 +60,6 @@ public class MouseManager implements MouseListener {
     @Override
     public void mouseReleased(MouseEvent mouseEvent) {
         gui.resetButtonEvent();
-        System.out.println("released");
     }
 
     /**
@@ -71,7 +69,6 @@ public class MouseManager implements MouseListener {
      */
     @Override
     public void mouseEntered(MouseEvent mouseEvent) {
-        System.out.println("entered");
         String name = mouseEvent.getComponent().getName();
 
         if (gui.getGameState() == 4) {
@@ -93,7 +90,5 @@ public class MouseManager implements MouseListener {
     @Override
     public void mouseExited(MouseEvent mouseEvent) {
         gui.resetButtonEvent();
-        System.out.println("why here?");
-        System.out.println("exited");
     }
 }
